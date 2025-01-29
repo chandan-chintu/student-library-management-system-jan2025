a544378e1590776327a8c1cdbb10ca1228624c4a
Chandan K M <chandanchintu16@gmail.com>
2025-01-29 22:40:27 +0530

    Transaction model class added

--- src/main/java/com/demo/example/student_library_management_system/model/Transaction.java
+++ src/main/java/com/demo/example/student_library_management_system/model/Transaction.java
@@ -1,4 +1,31 @@
 package com.demo.example.student_library_management_system.model;
 
+import com.demo.example.student_library_management_system.enums.TransactionType;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "transaction")
 public class Transaction {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "transaction_type",nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private TransactionType transactionType;
+
+    @Column(name = "transaction_date",nullable = false)
+    @CreationTimestamp
+    private Date transactionDate;
+
+    @Column(name = "due_date",nullable = false)
+    private String dueDate;
+
+    @Column(name = "fine",nullable = false)
+    private double fine;
 }

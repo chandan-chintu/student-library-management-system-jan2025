dc9a0905515140363810e325c5131ca7542df09e
Chandan K M <chandanchintu16@gmail.com>
2025-01-31 21:41:26 +0530

    Added save API for student, card and author

---
+++ src/main/java/com/demo/example/student_library_management_system/service/StudentService.java
@@ -0,0 +1,33 @@
+package com.demo.example.student_library_management_system.service;
+
+import com.demo.example.student_library_management_system.converter.StudentConverter;
+import com.demo.example.student_library_management_system.enums.CardStatus;
+import com.demo.example.student_library_management_system.model.Card;
+import com.demo.example.student_library_management_system.model.Student;
+import com.demo.example.student_library_management_system.repository.StudentRepository;
+import com.demo.example.student_library_management_system.requestdto.StudentRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public String saveStudent(StudentRequestDto studentRequestDto){
+
+        // first convert studentrequestdto into student object
+        Student student = StudentConverter.convertStudentRequestDtoIntoStudent(studentRequestDto);
+
+        //whenever the student are added card also issued/added at same time
+        Card card = new Card();
+        card.setCardStatus(CardStatus.ACTIVE);
+
+        student.setCard(card);
+        card.setStudent(student);
+
+        studentRepository.save(student);
+        return "Student and card saved successfully";
+    }
+}
